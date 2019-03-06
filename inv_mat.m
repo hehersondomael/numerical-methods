df450af59a676b18bb3feb1ba83725b3328f26a1
Heherson Domael <46311265+hehersondomael@users.noreply.github.com>
2019-03-06 23:47:29 +0800

    Create inv_mat.m
    Computes the inverse of matrix A.

---
+++ inv_mat.m
@@ -0,0 +1,55 @@
+## Copyright (C) 2019 Heherson Domael <h.domael@outlook.com>
+## 
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {[@var{Ainv}, @var{At}] =} inv_mat (@var{A})
+## Compute the inverse of matrix A.
+##
+## @var{Ainv} returns the inverse of A.
+##
+## @var{At} returns a table of column-by-column elementary row operations
+## performed to transform identity matrix into A^-1.
+## @seealso{inv}
+## @end deftypefn
+
+## Author: Heherson Domael <Heherson Domael@LAPTOP-MQALPEGV>
+## Created: 2019-03-06
+
+function [Ainv, At] = inv_mat (A)
+  if det(A)==0
+    sprintf("Input matrix is singular! Inverse does not exist.")
+    Adet = "N/A";
+    At = "N/A";
+  elseif rows(A) != columns(A)
+    sprintf("Input matrix must be square! Inverse does not exist.")
+    Adet = "N/A";
+    At = "N/A";
+  else
+    A = [eye(rows(A)) A];
+    for col=1+(columns(A)/2):columns(A)
+      for row=1:rows(A)
+        if row+rows(A)==col
+          A(row,:) = A(row,:)/A(row,col);
+          continue
+        else
+          A(row,:) -= A(row,col)*A(col-(columns(A)/2),:)/A(col-(columns(A)/2),col);
+        endif
+      endfor
+      At((col-(columns(A)/2))*rows(A)-rows(A)+1:(col-(columns(A)/2))*rows(A), 1:(columns(A)/2)) = A(1:rows(A),1:columns(A)/2);
+    endfor
+    Ainv = At((col-(columns(A)/2))*rows(A)-rows(A)+1:rows(At),:);
+  endif
+endfunction
